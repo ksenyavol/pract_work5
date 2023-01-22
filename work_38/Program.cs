fd00f958d7df1d203d524891ad153ddd7a66238f
git_pract <noxi1992@mail.ru>
2023-01-22 22:04:12 +0300

    добавили 5 задание

---
+++ work_38/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.7; 7.2; 2.1; 7.8] -> 5.7
+
+Console.WriteLine("Введите длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] numbers = new double[size];
+FillArrayWithRandom(numbers);
+Console.WriteLine("Наш массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+    for(int z = 0; z < numbers.Length; z++)
+    {
+        if (numbers[z] > max)
+    {
+        max = numbers[z];
+    }
+    if (numbers[z] < min)
+    {
+        min = numbers[z];
+    }
+    }
+Console.WriteLine($"всего {numbers.Length} чисел. Максимальное значение = {max} , минимальное значение = {min}");
+Console.WriteLine($"разница между максимальным и минимальным значением = {max - min}");
+
+void FillArrayWithRandom(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(10,100)) / 10;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.WriteLine(numbers[i] + " ");
+    }
+    Console.Write("] ");
+    Console.WriteLine();
+}
